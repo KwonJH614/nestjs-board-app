e86451055f2e90d34329b911396ee0f9ffa9e4d7
madogkwon <a93081077a@gmail.com>
2025-05-07 00:37:23 +0900

    refactor : ENV 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,34 @@
 import { Module } from '@nestjs/common';
-import { BoardsModule } from './boards/boards.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeormORMConfig } from './configs/typeorm.config';
-import { BoardRepository } from './boards/board.repository';
+import { BoardsModule } from './boards/boards.module';
 import { AuthModule } from './auth/auth.module';
+import { typeormORMConfig } from './configs/typeorm.config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([BoardRepository]),
-    TypeOrmModule.forRoot(typeormORMConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.string().valid('postgres').required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_ENTITIES: Joi.string().required(),
+        DATABASE_SYNCHRONIZE: Joi.boolean().default(true),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeormORMConfig,
+      inject: [ConfigService],
+    }),
     BoardsModule,
-    AuthModule
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,20 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
-export const typeormORMConfig : TypeOrmModuleOptions = {
-  type : 'postgres',
-  host : 'localhost',
-  port : 5432,
-  username : 'postgres',
-  password : '1234',
-  database : 'board-app',
-  entities : [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize : true
-}
+export const typeormORMConfig = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => {
+  const entitiesPath = configService.get<string>('DATABASE_ENTITIES');
+  if (!entitiesPath) {
+    throw new Error('DATABASE_ENTITIES is not defined in .env file');
+  }
+
+  return {
+    type: configService.get<string>('DATABASE_TYPE') as 'postgres',
+    host: configService.get<string>('DATABASE_HOST'),
+    port: configService.get<number>('DATABASE_PORT'),
+    username: configService.get<string>('DATABASE_USERNAME'),
+    password: configService.get<string>('DATABASE_PASSWORD'),
+    database: configService.get<string>('DATABASE_NAME'),
+    entities: [entitiesPath],
+    synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'),
+  };
+};
